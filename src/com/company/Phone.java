d9689d467d066898956e74cca1d20c6038693bcf
anton <anton>
2017-10-01 09:46:42 +0300

    1234

--- src/com/company/Phone.java
+++ src/com/company/Phone.java
@@ -1,6 +1,10 @@
 package com.company;
 
 public class Phone {
-    public static void receiveCall(String name){}
-    public static void receiveCall(long numer){}
+    public static void receiveCall(String name){
+        System.out.println(name);
+    }
+    public static void receiveCall(long numer){
+        System.out.println(numer);
+    }
 }
